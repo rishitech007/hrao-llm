2eeaee4fde15e5ecb50491fe6992a615ac5ca6b8
rishitech007 <rishi.gatech@gmail.com>
2024-03-11 21:44:06 +0000

    Changes

--- chatbot_hr_llm.py
+++ chatbot_hr_llm.py
@@ -25,20 +25,20 @@ import os
 
 st.set_page_config(layout = "wide")
 
-with st.sidebar:
-    DOCS_DIR = os.path.abspath("./uploaded_docs")
-    if not os.path.exists(DOCS_DIR):
-        os.makedirs(DOCS_DIR)
-    st.subheader("Add to the Knowledge Base")
-    with st.form("my-form", clear_on_submit=True):
-        uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files = True)
-        submitted = st.form_submit_button("Upload!")
-
-    if uploaded_files and submitted:
-        for uploaded_file in uploaded_files:
-            st.success(f"File {uploaded_file.name} uploaded successfully!")
-            with open(os.path.join(DOCS_DIR, uploaded_file.name),"wb") as f:
-                f.write(uploaded_file.read())
+#with st.sidebar:
+#    DOCS_DIR = os.path.abspath("./uploaded_docs")
+#    if not os.path.exists(DOCS_DIR):
+#        os.makedirs(DOCS_DIR)
+#    st.subheader("Add to the Knowledge Base")
+#    with st.form("my-form", clear_on_submit=True):
+#        uploaded_files = st.file_uploader("Upload a file to the Knowledge Base:", accept_multiple_files = True)
+#        submitted = st.form_submit_button("Upload!")
+#
+#    if uploaded_files and submitted:
+#        for uploaded_file in uploaded_files:
+#            st.success(f"File {uploaded_file.name} uploaded successfully!")
+#            with open(os.path.join(DOCS_DIR, uploaded_file.name),"wb") as f:
+#                f.write(uploaded_file.read())
 
 ############################################
 # Component #2 - Embedding Model and LLM
@@ -58,41 +58,21 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.vectorstores import FAISS
 import pickle
 
-with st.sidebar:
-    # Option for using an existing vector store
-    use_existing_vector_store = st.radio("Use existing vector store if available", ["Yes", "No"], horizontal=True)
+#with st.sidebar:
+#    # Option for using an existing vector store
+#    use_existing_vector_store = st.radio("Use existing vector store if available", ["Yes", "No"], horizontal=True)
 
 # Path to the vector store file
 vector_store_path = "vectorstore.pkl"
 
 # Load raw documents from the directory
+DOCS_DIR = os.path.abspath("./uploaded_docs")
 raw_documents = DirectoryLoader(DOCS_DIR).load()
-
-
-# Check for existing vector store file
-vector_store_exists = os.path.exists(vector_store_path)
-vectorstore = None
-if use_existing_vector_store == "Yes" and vector_store_exists:
-    with open(vector_store_path, "rb") as f:
-        vectorstore = pickle.load(f)
-    with st.sidebar:
-        st.success("Existing vector store loaded successfully.")
-else:
-    with st.sidebar:
-        if raw_documents:
-            with st.spinner("Splitting documents into chunks..."):
-                text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
-                documents = text_splitter.split_documents(raw_documents)
-
-            with st.spinner("Adding document chunks to vector database..."):
-                vectorstore = FAISS.from_documents(documents, document_embedder)
-
-            with st.spinner("Saving vector store"):
-                with open(vector_store_path, "wb") as f:
-                    pickle.dump(vectorstore, f)
-            st.success("Vector store created and saved.")
-        else:
-            st.warning("No documents available to process!", icon="⚠️")
+text_splitter = CharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
+documents = text_splitter.split_documents(raw_documents)
+vectorstore = FAISS.from_documents(documents, document_embedder)
+with open(vector_store_path, "wb") as f:
+    pickle.dump(vectorstore, f)
 
 ############################################
 # Component #4 - LLM Response Generation and Chat
